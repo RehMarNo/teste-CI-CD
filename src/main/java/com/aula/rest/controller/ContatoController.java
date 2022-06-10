4831cecf31cd7be7493c7da58643dc896a15a416
Nobrega <renatamarquesdl@gmail.com>
2022-06-10 09:29:23 -0300

    build no travis

--- src/main/java/com/aula/rest/controller/ContatoController.java
+++ src/main/java/com/aula/rest/controller/ContatoController.java
@@ -12,6 +12,6 @@ public class ContatoController {
 
 	@GetMapping
 	public ResponseEntity<String> getMsg(){
-		return ResponseEntity.status(HttpStatus.OK).body("Hello");
+		return ResponseEntity.status(HttpStatus.OK).body("Hello Guys");
 	}
 }
